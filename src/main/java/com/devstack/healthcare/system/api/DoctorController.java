83480667ff6c6b5a86090f49882c5ad23593bafc
tharaka <tharakawiduranga22@gmail.com>
2023-10-08 01:10:27 +0530

    spring boot day-1

--- src/main/java/com/devstack/healthcare/system/api/DoctorController.java
+++ src/main/java/com/devstack/healthcare/system/api/DoctorController.java
@@ -1,31 +1,47 @@
 package com.devstack.healthcare.system.api;
 
 import com.devstack.healthcare.system.dto.Request.RequestDoctorDto;
+import com.devstack.healthcare.system.service.DoctorService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/doctors")
 public class DoctorController {
+
+    private final DoctorService doctorService;
+
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
     @PostMapping
     public String CreateDoctor(@RequestBody RequestDoctorDto doctorDto){
-       return doctorDto.toString();
 
+     doctorService.createDoctor(doctorDto);
+     return doctorDto.toString();
 
     }
-    @GetMapping
-    public String FindDoctor() {
-        return "Doctor Found";
+    @GetMapping(path="/{id}")
+    public String FindDoctor(@PathVariable String id){
+
+        return id+"";
     }
-   @PutMapping
-    public String UpdateDoctor() {
-        return "Doctor Updated";
+   @PutMapping(params="id")
+    public String UpdateDoctor(
+            @RequestParam String id,
+            @RequestBody RequestDoctorDto doctorDto){
+        return doctorDto.toString();
     }
-   @DeleteMapping
-    public String DeleteDoctor() {
-        return "Doctor Deleted";
+   @DeleteMapping("/{id}")
+    public String DeleteDoctor(@PathVariable String id){
+        return id+"";
     }
-    @GetMapping(path="/all")
-    public String FindAllDoctor() {
+    @GetMapping(path="/list" , params={"searchText","page","size"})
+    public String FindAllDoctor(
+            @RequestParam String searchText,
+            @RequestParam Integer page,
+            @RequestParam Integer size
+    ) {
         return "All Doctors Found";
     }
 
