f0bc1c4f57606361f984d4e40d49673bec76126f
tharaka <tharakawiduranga22@gmail.com>
2023-10-09 02:15:03 +0530

    spring boot day-1-2

--- src/main/java/com/devstack/healthcare/system/api/DoctorController.java
+++ src/main/java/com/devstack/healthcare/system/api/DoctorController.java
@@ -2,6 +2,9 @@ package com.devstack.healthcare.system.api;
 
 import com.devstack.healthcare.system.dto.Request.RequestDoctorDto;
 import com.devstack.healthcare.system.service.DoctorService;
+import com.devstack.healthcare.system.utill.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,34 +18,47 @@ public class DoctorController {
     }
 
     @PostMapping
-    public String CreateDoctor(@RequestBody RequestDoctorDto doctorDto){
+    public ResponseEntity<StandardResponse> CreateDoctor(@RequestBody RequestDoctorDto doctorDto){
 
      doctorService.createDoctor(doctorDto);
-     return doctorDto.toString();
+     return new ResponseEntity<>(new StandardResponse
+             (201,"Doctor was saved",doctorDto.toString()), HttpStatus.CREATED);
+
 
     }
     @GetMapping(path="/{id}")
-    public String FindDoctor(@PathVariable String id){
-
-        return id+"";
+    public ResponseEntity<StandardResponse> FindDoctor(@PathVariable Long id){
+        return new ResponseEntity<>(
+                new StandardResponse
+                (200,"Doctor data",doctorService.getDoctor(id)), HttpStatus.OK);
     }
    @PutMapping(params="id")
-    public String UpdateDoctor(
-            @RequestParam String id,
+    public ResponseEntity<StandardResponse> UpdateDoctor(
+            @RequestParam Long id,
             @RequestBody RequestDoctorDto doctorDto){
-        return doctorDto.toString();
+
+       doctorService.updateDoctor(id,doctorDto);
+       return new ResponseEntity<>(new StandardResponse
+               (201,"Doctor updated",doctorDto.toString()), HttpStatus.CREATED);
+
+
     }
    @DeleteMapping("/{id}")
-    public String DeleteDoctor(@PathVariable String id){
-        return id+"";
+    public ResponseEntity<StandardResponse> DeleteDoctor(@PathVariable Long id){
+        doctorService.deleteDoctor(id);
+        return new ResponseEntity<>(new StandardResponse
+                (204,"Doctor deleted",id), HttpStatus.NO_CONTENT);
     }
     @GetMapping(path="/list" , params={"searchText","page","size"})
-    public String FindAllDoctor(
+    public ResponseEntity<StandardResponse> FindAllDoctor(
             @RequestParam String searchText,
             @RequestParam Integer page,
             @RequestParam Integer size
     ) {
-        return "All Doctors Found";
+        return new ResponseEntity<>(new StandardResponse
+                (200,"data list",doctorService.getAllDoctors(
+                        searchText,page,size
+                )), HttpStatus.OK);
     }
 
 }
